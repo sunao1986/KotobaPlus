bb4e1455a1ed856bff66d470d79cf65b1bbdeee5
sunao1986 <nk@sunao-products.com>
2019-12-14 13:50:53 +0900

    Initial Commit

---
+++ KotobaPlus/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KotobaPlus
+//
+//  Created by sunao on 2019/12/14.
+//  Copyright © 2019 sunao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
